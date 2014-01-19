9bb0b1e373a533d9b8ac660f066a8d1929dad5a3
Rui Dai <drstarry@gmail.com>
2014-01-18 23:39:23 -0800

    change settings

--- pluscrawl/settings.py
+++ pluscrawl/settings.py
@@ -15,8 +15,21 @@ ITEM_PIPELINES = ['pluscrawl.pipelines.PluscrawlPipeline',]
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'pluscrawl (+http://www.yourdomain.com)'
 import MySQLdb
+#USER_AGENT = 'myspace (+http://www.yourdomain.com)'
+# in
+MONGODB_SERVER = '10.1.1.111'
+#out
+#MONGODB_SERVER = ''
+MONGODB_PORT = 12345
+# MONGODB_SERVER = 'localhost'
+# MONGODB_PORT = 27017
+MONGODB_DB = 'lastfm'
+MONGODB_COLLECTION = 'profiles'
+MONGODB_UNIQ_KEY = '_id'
+##########
 
-
+# Set your own download folder
+# DOWNLOAD_FILE_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "download_file")
 
 USER_AGENT = "Mozilla/5.0 (Windows; U; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)"
 AUTOTHROTTLE_ENABLED= True
